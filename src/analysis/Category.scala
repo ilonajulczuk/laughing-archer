0d328f672f33068bd5b8f24077662bcde5525a15
Justyna Ilczuk <justyna.ilczuk@gmail.com>
2013-04-30 09:32:52 +0200

    Small refactoring of category tree.

--- src/analysis/Category.scala
+++ src/analysis/Category.scala
@@ -1,7 +1,5 @@
 package analysis
 
-
-
 class CategoryTree() {	
 	var root = new MultiBranch("Root")
 	override def toString(): String = {
@@ -12,9 +10,6 @@ class CategoryTree() {
 	}
 }
 
-
-
-
 sealed abstract class Node(val value: String) {
   /** Replaces this node or its children according to the given function */
   def replace(fn: Node => Node): Node
@@ -24,56 +19,28 @@ sealed abstract class Node(val value: String) {
     replace(n => if (n.value == value) node else n)
 }
 
-class Leaf(override val value: String) extends Node(value) {
-  /** Replaces this node or its children according to the given function */
-  def replace(fn: Node => Node): Node = {
-  	val newSelf = fn(this)
-  	newSelf
-  }
-}
-
-
-// putting value first makes class structure match tree structure
-class Branch(override val value: String, l: Node, r: Node)
-     extends Node(value) {
-	
-	var left = l
-	var right = r
-  def replace(fn: Node => Node): Node = {
-    val newSelf = fn(this)
-    if (this eq newSelf) {
-      val newLeft = left.replace(fn)
-      val newRight = right.replace(fn)
-
-      if ((left eq newLeft) && (right eq newRight)) {
-        this
-      } else {
-        left = newLeft
-        right = newRight
-        this
-      }
-    } else {
-      newSelf
-    }
-  }
-}
 
 import scala.collection.mutable.Map
 class MultiBranch(override val value: String)
      extends Node(value) {
 	
 	var children = Map[String, MultiBranch]()
+	
 	def replaceChild(name :String, newChild: MultiBranch)
 	{
 		children(name) = newChild
 	}
+	
 	def namesOfChildren() = children.keys
+	
 	def hasChildren(): Boolean = !children.isEmpty
+	
 	def addChild(child: MultiBranch){
 		if (children.keys.exists(x => x == child.value))
 				replaceChild(child.value, child)
 		children += (child.value -> child)
 	}
+	
 	def addChild(childName: String){
 		if (children.keys.exists(x => x == childName))
 				replaceChild(childName, new MultiBranch(childName))
