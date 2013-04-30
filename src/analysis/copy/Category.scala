9385b54b96bd6cd8cb9c2db3558e4a55c7e3ad3c
Justyna Ilczuk <justyna.ilczuk@gmail.com>
2013-04-30 17:01:24 +0200

    Refactoring of Categories structure.

---
+++ src/analysis/copy/Category.scala
@@ -0,0 +1,66 @@
+package analysis.copy
+
+class CategoryTree() {	
+	var root = new MultiBranch("Root")
+	override def toString(): String = {
+		var description = "Root"
+		description += root.namesOfChildren.mkString("\n")
+		//Algorithm isn't so easy...
+		description
+	}
+}
+
+sealed abstract class Node(val value: String) {
+  /** Replaces this node or its children according to the given function */
+  def replace(fn: Node => Node): Node
+
+  /** Helper to replace nodes that have a given value */
+  def replace(value: String, node: Node): Node =
+    replace(n => if (n.value == value) node else n)
+}
+
+
+import scala.collection.mutable.Map
+class MultiBranch(override val value: String)
+     extends Node(value) {
+	
+	var children = Map[String, MultiBranch]()
+	
+	def replaceChild(name :String, newChild: MultiBranch)
+	{
+		children(name) = newChild
+	}
+	
+	def namesOfChildren() = children.keys
+	
+	def hasChildren(): Boolean = !children.isEmpty
+	
+	def addChild(child: MultiBranch){
+		if (children.keys.exists(x => x == child.value))
+				replaceChild(child.value, child)
+		children += (child.value -> child)
+	}
+	
+	def addChild(childName: String){
+		if (children.keys.exists(x => x == childName))
+				replaceChild(childName, new MultiBranch(childName))
+		children += (childName -> new MultiBranch(childName))
+	}
+	
+	def addChildren(children: Iterable[String]) {
+		for(child <- children) {
+			addChild(child)
+		}
+	}
+	
+	def apply(childName: String) = children(childName)
+  def replace(fn: Node => Node): Node = {
+    val newSelf = fn(this)
+    if (this eq newSelf) {
+        this
+      
+    } else {
+      newSelf
+    }
+  }
+}
--- tests/test_analysis/CategoryTest.scala
+++ tests/test_analysis/CategoryTest.scala
@@ -1,7 +1,7 @@
 package test_analysis
 
 
-import analysis.{CategoryTree, Node, Leaf, MultiBranch}
+import analysis.{CategoryTree, Category}
 
 import org.scalatest.FunSuite
 import org.scalatest.BeforeAndAfter
@@ -22,16 +22,16 @@ class CategoryTest extends FunSuite with BeforeAndAfter {
   }
 	
 	test("Test building a tree") {
-		categoryTree.root.addChild(new MultiBranch("Science"))
-		categoryTree.root.addChild(new MultiBranch("Fiction"))
-		categoryTree.root.addChild(new MultiBranch("Documents"))
-		categoryTree.root.children("Science").addChildren(List("Physics",
+		categoryTree.root.addSubcategory(new Category("Science"))
+		categoryTree.root.addSubcategory(new Category("Fiction"))
+		categoryTree.root.addSubcategory(new Category("Documents"))
+		categoryTree.root.subcategories("Science").addSubcategories(List("Physics",
 				"Computer Science", "Math", "Biology"))
-		categoryTree.root.children("Fiction").addChildren(List("Fantasy", "Science-Fiction", "Horror"))	
-		categoryTree.root("Science")("Math").addChild("Calculus")
+		categoryTree.root.subcategories("Fiction").addSubcategories(List("Fantasy", "Science-Fiction", "Horror"))	
+		categoryTree.root("Science")("Math").addSubcategory("Calculus")
 		
 		println(categoryTree)
-		println(categoryTree.root("Fiction").namesOfChildren)
-		assert(categoryTree.root("Fiction").namesOfChildren.size === 3)
+		println(categoryTree.root("Fiction").namesOfSubcategories)
+		assert(categoryTree.root("Fiction").namesOfSubcategories.size === 3)
   }
 }
