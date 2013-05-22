4c3234ad0929c1574eb3df50ddb0dcc3afd45a6f
Justyna Ilczuk <justyna.ilczuk@gmail.com>
2013-05-22 13:01:43 +0200

    Parsing odt

---
+++ tests/odt/OdtParsingTest.scala
@@ -0,0 +1,17 @@
+package odt
+
+import org.scalatest.FunSuite
+import org.scalatest.BeforeAndAfter
+
+class OdtParsingTest extends FunSuite {
+	test("Read test file") {
+	  val parser = new OpenOfficeParser
+	  val testText = parser.getText("/home/att/eclipse/MyProject/resources/test.odt")
+	  assert(testText === "This is a test")
+	}
+	test("Read more complicated test file") {
+	  val parser = new OpenOfficeParser
+	  val testText = parser.getText("/home/att/eclipse/MyProject/resources/epr.odt")
+	  println(testText)
+	}
+}
