4a3cd112fc5f1a3f554820011d0e8a0da4005758
Justyna Ilczuk <justyna.ilczuk@gmail.com>
2013-05-29 18:20:10 +0200

    Removal of unneccessary files

--- src/Jack.scala
+++
@@ -1,6 +0,0 @@
-object Jack {
-
-  def main(args: Array[String]) : Unit = {
-    println("Scala roks, woooho!")
-  }
-}
--- src/analysis/BreezeCrap.scala
+++
@@ -1,7 +0,0 @@
-package analysis
-import breeze.text.tokenize._
-import breeze.data.Example
-
-class BreezeCrap {
- val langData = breeze.text.LanguagePack.English
-}
